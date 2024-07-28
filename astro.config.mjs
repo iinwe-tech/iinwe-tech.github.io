ad5937bf70323a0fd595db34f141a0418fab21f2
유성실 <yooo1201@naver.com>
2024-07-28 11:39:45 +0900

    chore: base 설정

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  base: '/',
   output: 'static',
   integrations: [tailwind()],
   trailingSlash: 'never',
