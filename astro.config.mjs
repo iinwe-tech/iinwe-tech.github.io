439f731e9f1cb0ae77d9ccdfa7f871178e646169
유성실 <yooo1201@naver.com>
2024-07-29 21:27:09 +0900

    feat: sitemap 추가

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   base: '/',
   output: 'static',
-  integrations: [tailwind()],
+  site: 'https://iinwe.org',
+  integrations: [
+    tailwind(),
+    sitemap(),
+  ],
   trailingSlash: 'never',
 });
